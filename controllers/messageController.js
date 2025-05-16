50b311ca4f5f5ccdac0fe56639fdbb76e6c288d6
selmahi.mail@gmail.com <Soukaina@air-de-loic.home>
2025-05-16 18:50:17 +0200

    adding Custom Not FOund Errors handling

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,8 +1,13 @@
 const { getMessageByIndex } = require("../dataBase");
+const CustomNotFoundError = require("../errors/customNotFoundError");
 
 async function handleGetMessageByIndex(req, res) {
   const index = Number(req.params.id);
   const message = await getMessageByIndex(index);
+
+  if (!message) {
+    throw new CustomNotFoundError("Message not found");
+  }
 
   res.render("message", { message: message });
 }
