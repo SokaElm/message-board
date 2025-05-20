47a683966b932c62ce7ef5ccdf2b288184505d24
selmahi.mail@gmail.com <Soukaina@air-de-loic.home>
2025-05-20 19:21:44 +0200

    updating port number

--- errors/customNotFoundError.js
+++ errors/customNotFoundError.js
@@ -1,6 +1,10 @@
 class CustomNotFoundError extends Error {
   constructor(message) {
     super(message);
+
+    if ("captureStackTrace" in Error) {
+      Error.captureStackTrace(this, CustomNotFoundError);
+    }
     this.statusCode = 404;
     this.name = "NotFoundError";
   }
