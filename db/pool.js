2b86e333cb3b98916b51e7c2f373373cbb3c8677
selmahi.mail@gmail.com <Soukaina@air-de-loic.home>
2025-05-20 19:56:00 +0200

    final working version

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 module.exports = new Pool({
   connectionString: process.env.CONNECTIONSTRING_PROD,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
